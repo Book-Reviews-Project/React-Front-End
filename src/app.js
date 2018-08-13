449af8e81eec59057ab4f3249870fbe08498f3f9
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2018-08-13 14:20:21 +0300

    added eslint rules

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 
 function app (props) {
-  return <h1>Hi</h1>;
+  return {
+    
+  };
 }
 
 export default hot(module)(app);
