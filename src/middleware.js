fb6be1e2523035e114f76b0754bc89cca8ba8f93
Emiliyan Panagyurski <emiliyan.panagyurski@techhuddle.com>
2018-11-27 14:10:43 +0200

    initial confoguration

---
+++ src/middleware.js
@@ -0,0 +1,12 @@
+import { CreateSagaMiddleware } from 'redux-saga';
+
+export const sagaMiddleware = CreateSagaMiddleware();
+
+const middleware = [sagaMiddleware];
+
+if (process.env.NODE_ENV = 'development') {
+  const { createLogger } = require('redux-logger');
+  middleware.push(createLogger({collapsed: true}));
+}
+
+export default middleware;
